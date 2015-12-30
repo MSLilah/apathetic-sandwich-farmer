7744095ac0e50ce87c78e606bb1acca0f65d3b36
Brian Cain <brian.cain@puppetlabs.com>
2015-12-30 15:01:25 -0800

    Add scripts folder and basic board generation scripts

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoardManager : MonoBehaviour {
+
+  private List<Vector3> gridPositions = new List<Vector3>();
+
+  private int columns = 10;
+  private int rows = 9;
+
+  void InitializeList() {
+    gridPositions.Clear();
+
+    for (int i  = 0; i <= columns; i++) {
+      for (int j = 0; j <= rows; j++) {
+        gridPositions.Add(new Vector3(i, j, 0f));
+      }
+    }
+  }
+
+  void SetupBoard() {
+    InitializeList();
+
+  }
+
+  public void GenerateBoard() {
+    SetupBoard();
+
+  }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+  private BoardManager boardManager;
+
+  void InitGame() {
+    boardManager.GenerateBoard();
+  }
+
+  void Update() {
+
+  }
+
+  void GameOver() {
+
+  }
+}
