903f28950037372da519072c90c30c19fae1c7ad
Brian Cain <brianccain@gmail.com>
2015-12-30 20:54:12 -0800

    Remove debug methods

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -39,50 +39,40 @@ public class BoardManager : MonoBehaviour {
   }
 
   GameObject DetermineTile(int i, int j) {
-    Debug.Log("DetermineTile: " + i + "," + j);
     if (i == 0 && j == 0) {
       // wall lower left
-      //Debug.Log("Wall Lower Left");
       return WallLowerLeft;
     }
     else if (i == 0 && j == rows) {
       // wall Upper left
-      //Debug.Log("Wall Upper Left");
       return WallUpperLeft;
     }
     else if (i == columns && j == 0) {
       // wall lower right
-      //Debug.Log("Wall Lower Right");
       return WallLowerRight;
     }
     else if (i == columns && j == rows) {
       // wall upper right
-      //Debug.Log("Wall Lower Right");
       return WallUpperRight;
     }
     else if (i == 0 && j < rows) {
       // wall left
-      //Debug.Log("Wall Left");
       return WallLeft;
     }
     else if (i < columns  && j == 0) {
       // wall lower
-      Debug.Log("Wall Lower");
       return WallLower;
     }
     else if (i == columns && j < rows) {
       // wall right
-      //Debug.Log("Wall Right");
       return WallRight;
     }
     else if (i > 0 && j == rows) {
       // wall Upper
-      Debug.Log("Wall Upper");
       return WallUpper;
     }
     else {
       // Floor_1
-      Debug.Log("Floor 1");
       return Floor1;
     }
   }
