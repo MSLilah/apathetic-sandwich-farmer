3012caf2168f3803684fdda958a955c4bb2a059b
Preben Ingvaldsen <preben.ingvaldsen@gmail.com>
2015-12-30 19:24:27 -0800

    Create basic player object
    Create a basic player object with the ability to move on the
    screen.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+  public float playerSpeed;
+
+  private Rigidbody2D rb;
+
+  void Start() {
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
+	// Update is called once per frame
+	void Update () {
+    Move ();
+	}
+
+  private void Move() {
+    float velocityX = Input.GetAxisRaw ("Horizontal") * playerSpeed;
+    float velocityY = Input.GetAxisRaw ("Vertical") * playerSpeed;
+
+    rb.velocity = new Vector2 (velocityX, velocityY);
+  }
+}
