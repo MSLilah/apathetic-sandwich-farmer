a4776be4d84fcfa5cac5f2f43adea64274f82295
Brian Cain <brianccain@gmail.com>
2015-12-30 22:57:34 -0800

    Add basic enemy gameobject

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+
+public class Enemy : MonoBehaviour {
+
+  private Rigidbody2D rb;
+
+  void Start() {
+    rb = gameObject.GetComponent<Rigidbody2D>();
+  }
+
+  void Update() {
+    Move();
+  }
+
+  void OnTriggerEnter2D(Collider2D coll) {
+    rb.velocity = Vector2.zero;
+  }
+
+  void OnTriggerStay2D(Collider2D coll) {
+    rb.velocity = Vector2.zero;
+  }
+
+  private void Move() {
+    float locationX = Random.Range(0, 9);
+    float locationY = Random.Range(0, 8);
+    Vector2 target = new Vector2(4,4);
+
+    //transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), target, 3 * Time.deltaTime);
+  }
+}
