b578113862266efbe727255c4fba8e3730426243
Preben Ingvaldsen <preben@puppetlabs.com>
2016-01-05 20:53:38 -0800

    Add Link sprites
    Add Link Sprites and basic walking animation.

--- Assets/Scripts/Actor.cs
+++ Assets/Scripts/Actor.cs
@@ -4,10 +4,12 @@ using System.Collections;
 public abstract class Actor : MonoBehaviour {
 
   private Rigidbody2D rb;
+  private Animator anim;
 
   // Use this for initialization
   protected virtual void Start () {
     rb = gameObject.GetComponent<Rigidbody2D>();
+    anim = gameObject.GetComponent<Animator>();
   }
   // Update is called once per frame
   void Update () {
@@ -24,9 +26,16 @@ public abstract class Actor : MonoBehaviour {
   }
 
   protected virtual void Move(float velocityX, float velocityY) {
-    rb.velocity = new Vector2 (velocityX, velocityY);
+    Vector2 velocity = new Vector2(velocityX, velocityY);
+    rb.velocity = velocity;
+
+    anim.SetBool("Moving", velocity.magnitude > 0);
   }
 
   protected virtual void Attack() {
+  }
+
+  protected Animator GetAnim() {
+    return anim;
   }
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Art: [16x16 Fantasy Tileset by Jerom](http://opengameart.org/content/16x16-fantasy-tileset)
+
+All other sprites from The Legend of Zelda: Link's Awakening DX by Nintendo. Sprites ripped by Bruce Juice.
